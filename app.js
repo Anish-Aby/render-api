e42a6489a09dab8f80f1f72fc06d8a85f034fb78
Anish-Aby <anishaby963@gmail.com>
2024-01-26 13:43:31 +0530

    Include options in cors.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors());
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
