2c101d92479f697f940529473a3870599a972e2e
Anish-Aby <anishaby963@gmail.com>
2024-01-26 13:40:44 +0530

    Add config for cors.

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const morgan = require("morgan");
 
 app.use(express.json());
 app.use(express.static("public"));
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors());
 
 if (process.env.NODE_ENV === "development") {
